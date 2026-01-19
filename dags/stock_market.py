925264e4990634598800806355f49007c6303b53
Jose Roque Quintanilla Nolazco <jrquintanilla@deacerousa.com>
2026-01-19 16:50:00 -0600

    class-s3-27: Updating stock markets pyfor fetching

--- dags/stock_market.py
+++ dags/stock_market.py
@@ -1,7 +1,12 @@
 from airflow.decorators import dag, task
 from airflow.hooks.base import BaseHook
 from airflow.sensors.base import PokeReturnValue
+from airflow.operators.python import PythonOperator
 from datetime import datetime
+from include.stock_market.tasks import _get_stock_prices
+
+
+SYMBOL = 'AAPL'
 
 @dag(
     start_date=datetime(2023, 1, 1),
@@ -22,7 +27,13 @@ def stock_market():
         response = requests.get(url,headers=api.extra_dejson['headers'])
         condition = response.json()['finance']['result'] is None
         return PokeReturnValue(is_done=condition, xcom_value=url)
-    
-    is_api_available()
+
+    get_stock_prices = PythonOperator(
+        task_id='get_stock_prices',
+        python_callable=_get_stock_prices,
+        op_kwargs={'url': '{{ ti.xcom_pull(task_ids="is_api_available") }}', 'symbol': SYMBOL}
+    )
+
+    is_api_available() >> get_stock_prices
 
 stock_market()
