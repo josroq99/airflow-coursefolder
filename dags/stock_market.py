ac38fbaf322cdab776e321ce27bd29fc1c386732
Jose Roque Quintanilla Nolazco <jrquintanilla@deacerousa.com>
2026-01-26 17:04:27 -0600

    class-s3-30:Getting Formatted CSV

--- dags/stock_market.py
+++ dags/stock_market.py
@@ -3,8 +3,8 @@ from airflow.hooks.base import BaseHook
 from airflow.sensors.base import PokeReturnValue
 from airflow.operators.python import PythonOperator
 from datetime import datetime
-from include.stock_market.tasks import _get_stock_prices, _store_prices
-
+from include.stock_market.tasks import _get_stock_prices, _store_prices, _get_formatted_csv
+from airflow.providers.docker.operators.docker import DockerOperator
 
 SYMBOL = 'AAPL'
 
@@ -40,6 +40,29 @@ def stock_market():
         op_kwargs={'stock': '{{ ti.xcom_pull(task_ids="get_stock_prices") }}'}
     )
 
-    is_api_available() >> get_stock_prices >> store_prices
+    format_prices = DockerOperator(
+        task_id='format_prices',
+        image='airflow/stock-app',
+        container_name='format_prices',
+        api_version='auto',
+        auto_remove='success',
+        docker_url='tcp://host.docker.internal:2376',
+        network_mode='container:spark-master',
+        tty=True,
+        xcom_all=False,
+        mount_tmp_dir=False,
+        environment={
+            'SPARK_APPLICATION_ARGS': '{{ ti.xcom_pull(task_ids="store_prices") }}'
+
+        }
+    )
+
+    get_formatted_csv = PythonOperator(
+        task_id='get_formatted_csv',
+        python_callable=_get_formatted_csv,
+        op_kwargs={'path': '{{ ti.xcom_pull(task_ids="store_prices") }}'}
+    )
+
+    is_api_available() >> get_stock_prices >> store_prices >> format_prices >> get_formatted_csv
 
 stock_market()
