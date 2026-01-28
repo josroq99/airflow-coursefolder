94e6891eb6b97c0223325c2c7679978e35e5a578
Jose Roque Quintanilla Nolazco <jrquintanilla@deacerousa.com>
2026-01-28 14:23:45 -0600

    class-s3-31:Load to PostgressDW

--- dags/stock_market.py
+++ dags/stock_market.py
@@ -3,8 +3,12 @@ from airflow.hooks.base import BaseHook
 from airflow.sensors.base import PokeReturnValue
 from airflow.operators.python import PythonOperator
 from datetime import datetime
-from include.stock_market.tasks import _get_stock_prices, _store_prices, _get_formatted_csv
+from include.stock_market.tasks import _get_stock_prices, _store_prices, _get_formatted_csv, BUCKET_NAME
 from airflow.providers.docker.operators.docker import DockerOperator
+from astro import sql as aql
+from astro.files import File
+from astro.sql.table import Table, Metadata
+
 
 SYMBOL = 'AAPL'
 
@@ -63,6 +67,25 @@ def stock_market():
         op_kwargs={'path': '{{ ti.xcom_pull(task_ids="store_prices") }}'}
     )
 
-    is_api_available() >> get_stock_prices >> store_prices >> format_prices >> get_formatted_csv
+    load_to_dw = aql.load_file(
+        task_id='load_to_dw',
+        input_file=File(
+            path=f"s3://{{{{ ti.xcom_pull(task_ids='get_formatted_csv')}}}}",
+            conn_id='minio'
+        ),
+        output_table=Table(
+            name='stock_market',
+            conn_id='postgres',
+            metadata=Metadata(schema='public')
+        ),
+        load_options={
+            "aws_access_key_id": BaseHook.get_connection('minio').login,
+            "aws_secret_access_key": BaseHook.get_connection('minio').password,
+            "endpoint_url": BaseHook.get_connection('minio').host,
+
+        }
+    )
+
+    is_api_available() >> get_stock_prices >> store_prices >> format_prices >> get_formatted_csv >> load_to_dw
 
 stock_market()
